6b1c64a8488ef4645827425fc8b564bae4559937
Aneesh Durg <aneeshdurg17@gmail.com>
2020-07-29 17:43:11 -0700

    Webhook support

--- webhook/deployer.sh
+++ webhook/deployer.sh
@@ -1,4 +1,3 @@
 set -ex
 
-cd /home/pi/ephemeral/
 su pi -c "bash -i webhook/do_deploy.sh"
--- webhook/server.py
+++ webhook/server.py
@@ -1,4 +1,6 @@
 #!/usr/local/bin/python3
+import os
+import subprocess
 
 from flask import Flask, request
 app = Flask(__name__)
@@ -9,15 +11,20 @@ def hook():
         return "hello!"
     else:
         # Trigger deploy
-
+        try:
+            req = request.get_json()
+            branch = req['ref'].split('/')[-1]
+            if branch == "main":
+                subprocess.check_call(["bash", "webhook/deployer.sh"])
+        except:
+            pass
         return ('', 204)
 
 if __name__ == "__main__":
+    os.chdir(os.path.dirname(__file__))
+    os.chdir("..")
+
     app.run(
         host="0.0.0.0",
         port=9002,
-        # ssl_context=(
-        #     '/etc/letsencrypt/live/aneeshdurg.ddns.net/cert.pem',
-        #     '/etc/letsencrypt/live/aneeshdurg.ddns.net/privkey.pem'
-        # )
     )
