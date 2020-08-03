e8d362fe40162ff23586529d3fb2167c7c66daf1
Aneesh Durg <aneeshdurg17@gmail.com>
2020-08-02 19:34:03 -0700

    Remove deleted files during deploy

--- webhook/do_deploy.sh
+++ webhook/do_deploy.sh
@@ -10,12 +10,13 @@ git clone https://github.com/aneeshdurg/ephemeral ephemeral-live
 pushd ephemeral-live
 git remote set-url origin git@github.com:aneeshdurg/ephemeral
 git checkout gh-pages
-popd
 
-cp -r dist/* ephemeral-live/
-cd ephemeral-live
+rm -r *
+cp -r ../dist/* ephemeral-live/
+
 git add .
 git commit -m Updates --allow-empty
 git push origin gh-pages
+popd
 
 rm -rf ephemeral-live
